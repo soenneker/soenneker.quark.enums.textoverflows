6a29f5e3b8acb0ecb1d65899bbde6cbeab4480d8
Jake Soenneker <jake@soeneker.com>
2025-09-25 19:13:22 -0500

    Namespace updates

---
+++ src/TextOverflow.cs
@@ -0,0 +1,11 @@
+using Intellenum;
+
+namespace Soenneker.Quark;
+
+/// <summary>
+/// An HTML CSS keyword set in .NET enumeration form
+/// </summary>
+[Intellenum<string>]
+public partial class TextOverflow
+{
+}
---
+++ test/Soenneker.Quark.Enums.TextOverflows.Tests/TextOverflowTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.Textoverflows;
+
+public sealed class TextOverflowTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
