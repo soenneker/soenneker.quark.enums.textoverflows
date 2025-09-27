8e54e38d7cab619cf4ac5ee8862267cc4322b8ec
Jake Soenneker <jake@soeneker.com>
2025-09-27 09:14:15 -0500

    Renaming

---
+++ src/TextOverflowKeyword.cs
@@ -0,0 +1,14 @@
+using Intellenum;
+
+namespace Soenneker.Quark.Enums;
+
+/// <summary>
+/// An HTML CSS keyword set in .NET enumeration form
+/// </summary>
+[Intellenum<string>]
+public sealed partial class TextOverflowKeywordKeyword
+{
+    public static readonly TextOverflowKeyword Clip = new("clip");
+    public static readonly TextOverflowKeyword Ellipsis = new("ellipsis");
+    public static readonly TextOverflowKeyword String = new("string");
+}
